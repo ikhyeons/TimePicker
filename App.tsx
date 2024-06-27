fb58c0be4a3e567389e86ab9a0947dad7698ca7d
ikhyeons <skantrkwl789@naver.com>
2024-06-27 20:55:29 +0900

    Feat : 로딩 페이지 만들기

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import RootNavigator from "./navigator/RootNavigator";
 import { NavigationContainer } from "@react-navigation/native";
+import Loading from "./components/loading/Loading";
 export default function App() {
+  const [isLoadingEnd, setIsLoadingEnd] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoadingEnd(true);
+    }, 1000);
+  }, []);
+
+  if (!isLoadingEnd) {
+    return <Loading />;
+  }
   return (
     <NavigationContainer>
       <RootNavigator />
--- components/loading/Loading.tsx
+++ components/loading/Loading.tsx
@@ -1,7 +1,19 @@
 import React from "react";
-import { View } from "react-native";
+
+import { Text } from "react-native-paper";
+import styled from "styled-components/native";
+
+const View = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 const Loading = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Text>로딩 중...</Text>
+    </View>
+  );
 };
 
 export default Loading;
