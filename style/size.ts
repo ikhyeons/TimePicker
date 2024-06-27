9a4b0fe840e29772751d6344f34fe8a8fade27e9
ikhyeons <skantrkwl789@naver.com>
2024-06-27 20:56:17 +0900

    Feat : 스타일 사이즈 및 컬러 추가

---
+++ style/size.ts
@@ -0,0 +1,15 @@
+import { Dimensions } from "react-native";
+
+export const FULL_WIDTH = Dimensions.get("screen").width;
+export const FULL_HEIGHT = Dimensions.get("screen").height;
+export const HALF_WIDTH = FULL_WIDTH / 2;
+export const HALF_HEIGHT = FULL_HEIGHT / 2;
+export const TEXT_INPUT_lg = (FULL_WIDTH * 3) / 4;
+export const TEXT_INPUT_md = FULL_WIDTH / 2;
+export const TEXT_INPUT_sm = FULL_WIDTH / 3;
+
+export const BTN_lg = (FULL_WIDTH * 3) / 4;
+export const BTN_md = (FULL_WIDTH * 3) / 8 - 5;
+export const BTN_sm = FULL_WIDTH / 3 - 5;
+
+export default {};
