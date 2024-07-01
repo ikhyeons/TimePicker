efad58ea4a7c56be373852d828fa3aa7e06543bf
ikhyeons <skantrkwl789@naver.com>
2024-07-01 14:58:25 +0900

    Feat : 그룹관리 화면 구현

---
+++ components/dummyData/userGroup.ts
@@ -0,0 +1,15 @@
+export const userGroup = [
+  {
+    id: 1,
+    title: "용마고",
+    isOpen: false,
+    member: [{ name: "성익현" }, { name: "류창완" }, { name: "장민욱" }],
+  },
+  {
+    id: 2,
+    title: "314랩",
+    isOpen: false,
+    member: [{ name: "성익현" }, { name: "류창완" }, { name: "장민욱" }],
+  },
+];
+export default userGroup;
--- screens/main/Group.tsx
+++ screens/main/Group.tsx
@@ -1,7 +1,93 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import styled from "styled-components/native";
+import Accordion from "react-native-collapsible";
+import userGroup from "../../components/dummyData/userGroup";
+import user from "../../components/dummyData/user";
+import { FlatList } from "react-native";
+const View = styled.View``;
+const Text = styled.Text``;
+
+const Container = styled.View`
+  flex: 1;
+  flex-direction: row;
+`;
+const Left = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+`;
+const Right = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+`;
+const Line = styled.View`
+  border-right-width: 1px;
+  border-right-color: lightgrey;
+`;
+
+const Header = styled.View`
+  height: 50px;
+  background-color: red;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GroupBtn = styled.TouchableOpacity`
+  background-color: grey;
+`;
+const Pressable = styled.Pressable``;
 const Group = () => {
-  return <View></View>;
+  const [memberGroup, setMemberGroup] = useState(userGroup);
+  const [userList, setUserList] = useState(user);
+  return (
+    <Container>
+      <Left>
+        <Header>
+          <Text>프렌드 리스트</Text>
+        </Header>
+        <FlatList
+          data={userList}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.name}</Text>
+            </View>
+          )}
+        />
+      </Left>
+      <Line />
+      <Right>
+        <Header>
+          <Text>그룹 리스트</Text>
+        </Header>
+        <FlatList
+          data={memberGroup}
+          keyExtractor={(data) => `${data.id}`}
+          renderItem={({ item }) => (
+            <GroupBtn
+              onPress={() => {
+                const newArr = [...memberGroup];
+                newArr.map((newData) => {
+                  if (newData.id == item.id) newData.isOpen = !newData.isOpen;
+                });
+                setMemberGroup(newArr);
+              }}
+            >
+              <Text>
+                {item.title}
+                {item.isOpen ? "→" : "↓"}
+              </Text>
+              <Accordion collapsed={item.isOpen}>
+                <Pressable>
+                  {item.member.map((data: any, i2: number) => (
+                    <Text key={i2}>-{data.name}</Text>
+                  ))}
+                </Pressable>
+              </Accordion>
+            </GroupBtn>
+          )}
+        />
+      </Right>
+    </Container>
+  );
 };
 
 export default Group;
