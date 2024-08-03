fd2a2fd78c87173c8a0eaf972651d2b39c26d348
ikhyeons <skantrkwl789@naver.com>
2024-08-04 01:58:56 +0900

    Feat : 회원가입 API 연동

--- apis/memberApi.ts
+++ apis/memberApi.ts
@@ -16,15 +16,26 @@ export function login(username: string, password: string) {
 }
 
 export function joinMember(id: string, password: string, name: string) {
-  return axios.post(
-    `${BASE_URL}/member/join`,
-    {
+  return fetch(`${BASE_URL}/member/join`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
       mid: id,
       password,
       name,
-    },
-    { withCredentials: true }
-  );
+    }),
+    credentials: "include",
+  })
+    .then((res) => res.json())
+    .then((res) => res);
+}
+
+export function checkDuplicate(memberId: string) {
+  return fetch(`${BASE_URL}/member/checkDuplicate?mid=${memberId}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  });
 }
 
 export default {};
